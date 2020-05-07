6042fca42060ccb50e9eb74d770913c49e3f14e4
kin-au <kinnamau@gmail.com>
2020-05-07 21:21:18 +0100

    update cv and fix navbar link colour

--- src/containers/CV/CV.js
+++ src/containers/CV/CV.js
@@ -1,7 +1,7 @@
 import React from "react";
 import * as SC from "./CV.style";
 import download from "../../assets/icons/download.svg";
-import cv from "../../assets/cv/20200410-kin-au-cv-website.pdf";
+import cv from "../../assets/cv/20200507-kin-au-cv-website.pdf";
 
 const CV = () => {
   return (
